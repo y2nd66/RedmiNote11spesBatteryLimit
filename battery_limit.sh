60fb67aa93b1f262783f4a36c6cde833182e8d6e
y2nd66 <77805980+y2nd66@users.noreply.github.com>
2023-08-02 16:13:15 -0400

    Add files via upload

--- battery_limit.sh
+++ battery_limit.sh
@@ -5,57 +5,58 @@ MAX=80
 CONTROL_FILE=/sys/class/power_supply/battery/input_suspend
 CAPACITY=/sys/class/power_supply/battery/capacity
 REFRESH_INTERVAL=15
-THEFILE=thefile
+VAR_FILE=var_file
 
-if [ -e $THEFILE ]
+if [ -e $VAR_FILE ]
 then
-   rm $THEFILE
-   echo $THEFILE ' is removed'
+   rm $VAR_FILE
+   echo $VAR_FILE ' is removed'
 fi
 
 while true
 do
   CURRENT_LEVEL=$(cat $CAPACITY)
   echo 'Battery level: ' $CURRENT_LEVEL
-
-  if [ $CURRENT_LEVEL -gt $MAX ]
-  then
-    echo "1" > $CONTROL_FILE
-    echo 'Suspending charging'
-    THEVALUE=$(cat $CONTROL_FILE)
-    echo 'Result:' $THEVALUE
-    if ! [ -e $THEFILE ]
-    then
-       echo -n "" > $THEFILE
-    fi  
-  fi
   
-  if [ $CURRENT_LEVEL -ge $MIN && $CURRENT_LEVEL -le $MAX ]
+  if [ $CURRENT_LEVEL -lt $MIN && -e $VAR_FILE ]
   then
-    if [ -e $THEFILE ]
-    then
-       echo "1" > $CONTROL_FILE
-       echo 'Suspending charging'
-       THEVALUE=$(cat $CONTROL_FILE)
-       echo 'Range:' $THEVALUE
-    fi  
-  fi
-
-  if [ $CURRENT_LEVEL -lt $MIN ]
-  then
+    rm $VAR_FILE
+    echo $VAR_FILE ' is removed'
+    
     echo "0" > $CONTROL_FILE
     echo 'Resuming charging'
+    
     THEVALUE=$(cat $CONTROL_FILE)
-    echo 'Result:' $THEVALUE
-    if [ -e $THEFILE ]
+    echo 'Control:' $THEVALUE    
+  else
+    if [ $CURRENT_LEVEL -gt $MAX ]
     then
-       rm $THEFILE
-       echo $THEFILE ' is removed'
+      echo "1" > $CONTROL_FILE
+      echo 'Suspending charging'
+      
+      THEVALUE=$(cat $CONTROL_FILE)
+      echo 'Control:' $THEVALUE
+      
+      if ! [ -e $VAR_FILE ]
+      then
+         echo -n "" > $VAR_FILE
+      fi  
+    else
+      # $CURRENT_LEVEL -ge $MIN && $CURRENT_LEVEL -le $MAX
+      if [ -e $VAR_FILE ]
+      then
+         echo "1" > $CONTROL_FILE
+         echo 'Suspending charging'
+         
+         THEVALUE=$(cat $CONTROL_FILE)
+         echo 'Control:' $THEVALUE
+      fi  
     fi
-  fi
+  fi  
 
   echo 'Wait...' $REFRESH_INTERVAL 's'
   sleep $REFRESH_INTERVAL
+  
   THEVALUE=$(cat $CONTROL_FILE)
-  echo 'Now:' $THEVALUE
+  echo 'Control:' $THEVALUE
 done
