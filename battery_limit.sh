0fe1801450446c2a969024a985936e8327a924ba
y2nd66 <77805980+y2nd66@users.noreply.github.com>
2023-08-02 11:08:24 -0400

    Add files via upload

--- battery_limit.sh
+++ battery_limit.sh
@@ -1,23 +1,34 @@
-#!/data/data/com.termux/files/usr/bin/bash
+#!/system/bin/sh
+
 MIN=50
 MAX=80
 CONTROL_FILE=/sys/class/power_supply/battery/input_suspend
 CAPACITY=/sys/class/power_supply/battery/capacity
-REFRESH_INTERVAL=60
+REFRESH_INTERVAL=15
 
 while true
 do
   CURRENT_LEVEL=$(cat $CAPACITY)
   echo 'Battery level: ' $CURRENT_LEVEL
-  if [[ $CURRENT_LEVEL > $MAX ]]; then
+
+  if [ $CURRENT_LEVEL -gt $MAX ]
+  then
     echo "1" > $CONTROL_FILE
     echo 'Suspending charging'
+    THEVALUE=$(cat $CONTROL_FILE)
+    echo 'Result:' $THEVALUE
   fi
 
-  if [[ $CURRENT_LEVEL < $MIN ]]; then
+  if [ $CURRENT_LEVEL -lt $MIN ]
+  then
     echo "0" > $CONTROL_FILE
     echo 'Resuming charging'
+    THEVALUE=$(cat $CONTROL_FILE)
+    echo 'Result:' $THEVALUE
   fi
 
+  echo 'Wait...' $REFRESH_INTERVAL 's'
   sleep $REFRESH_INTERVAL
+  THEVALUE=$(cat $CONTROL_FILE)
+  echo 'Now:' $THEVALUE
 done
