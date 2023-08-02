ad3c255668eaac86f4a4ce500fb3802383f2bfa3
y2nd66 <77805980+y2nd66@users.noreply.github.com>
2023-08-02 11:47:00 -0400

    Add files via upload

--- battery_limit.sh
+++ battery_limit.sh
@@ -5,6 +5,13 @@ MAX=80
 CONTROL_FILE=/sys/class/power_supply/battery/input_suspend
 CAPACITY=/sys/class/power_supply/battery/capacity
 REFRESH_INTERVAL=15
+THEFILE=thefile
+
+if [ -e $THEFILE ]
+then
+   rm $THEFILE
+   echo $THEFILE ' is removed'
+fi
 
 while true
 do
@@ -17,6 +24,21 @@ do
     echo 'Suspending charging'
     THEVALUE=$(cat $CONTROL_FILE)
     echo 'Result:' $THEVALUE
+    if ! [ -e $THEFILE ]
+    then
+       echo -n "" > $THEFILE
+    fi  
+  fi
+  
+  if [ $CURRENT_LEVEL -ge $MIN && $CURRENT_LEVEL -le $MAX ]
+  then
+    if [ -e $THEFILE ]
+    then
+       echo "1" > $CONTROL_FILE
+       echo 'Suspending charging'
+       THEVALUE=$(cat $CONTROL_FILE)
+       echo 'Range:' $THEVALUE
+    fi  
   fi
 
   if [ $CURRENT_LEVEL -lt $MIN ]
@@ -25,6 +47,11 @@ do
     echo 'Resuming charging'
     THEVALUE=$(cat $CONTROL_FILE)
     echo 'Result:' $THEVALUE
+    if [ -e $THEFILE ]
+    then
+       rm $THEFILE
+       echo $THEFILE ' is removed'
+    fi
   fi
 
   echo 'Wait...' $REFRESH_INTERVAL 's'
