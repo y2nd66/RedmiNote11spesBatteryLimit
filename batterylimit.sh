bdf5492befb8fc9d7ffee8c0d7e94507579a2b25
y2nd66 <77805980+y2nd66@users.noreply.github.com>
2023-08-11 12:06:27 -0400

    Add files via upload

--- batterylimit.sh
+++ batterylimit.sh
@@ -4,35 +4,39 @@ MIN=50
 MAX=80
 CONTROL_FILE=/sys/class/power_supply/battery/input_suspend
 CAPACITY_FILE=/sys/class/power_supply/battery/capacity
-POWERED_FILE=/sys/class/power_supply/battery/charge_type
 STATUS_FILE=/sys/class/power_supply/battery/status
-FILE_VAR=var_file
-INTERVAL_ON_VAR=10
+FILE_VAR=/data/adb/batterylimit_var
+POWERED_FILE=/data/adb/batterylimit_file
+INTERVAL_ON_VAR=5
 INTERVAL_OFF_VAR=60
 
-#if [ -e $FILE_VAR ]
-#then
-#   rm $FILE_VAR
-#   echo $FILE_VAR 'is removed'
-#fi
+# if [ -e $FILE_VAR ]
+# then
+#    rm $FILE_VAR
+#    echo $FILE_VAR 'is removed'
+# fi
 
 while true
-do
-  CONTROL_1_VAR=$(cat $CONTROL_FILE)
-  CAPACITY_VAR=$(cat $CAPACITY_FILE)  
+do  
+  dumpsys battery | grep "AC powered: true" > $POWERED_FILE
   POWERED_VAR=$(cat $POWERED_FILE)
+  POWERED_VAR=$(echo $POWERED_VAR)
+  # log -t batterylimit ${#POWERED_VAR}
     
-  if [[ $POWERED_VAR = "N/A" ]]
+  if [[ $POWERED_VAR = "0" ]]  
   then
-    INTERVAL_VAR=$INTERVAL_OFF_VAR
+    INTERVAL_VAR=$INTERVAL_OFF_VAR  
   else
+    CONTROL_1_VAR=$(cat $CONTROL_FILE)
+    CAPACITY_VAR=$(cat $CAPACITY_FILE)
     if [[ $CAPACITY_VAR -lt $MIN && -e $FILE_VAR ]]
     then
       rm $FILE_VAR    
       echo "0" > $CONTROL_FILE    
       CONTROL_2_VAR=$(cat $CONTROL_FILE)    
-      #echo $CAPACITY_VAR':['$CONTROL_1_VAR':'$CONTROL_2_VAR']' $FILE_VAR 'is removed, resuming charging...'
+      # echo $CAPACITY_VAR':['$CONTROL_1_VAR':'$CONTROL_2_VAR']' $FILE_VAR 'is removed, resuming charging...'
       log -t batterylimit $CAPACITY_VAR':['$CONTROL_1_VAR':'$CONTROL_2_VAR']' $FILE_VAR 'is removed, resuming charging...'
+      INTERVAL_VAR=$INTERVAL_OFF_VAR
     else
       if [ $CAPACITY_VAR -gt $MAX ]
       then
@@ -41,30 +45,32 @@ do
         if ! [ -e $FILE_VAR ]
         then
            echo -n "" > $FILE_VAR         
-           #echo $CAPACITY_VAR':['$CONTROL_1_VAR':'$CONTROL_2_VAR']' $FILE_VAR 'is created, suspending charging...'
+           # echo $CAPACITY_VAR':['$CONTROL_1_VAR':'$CONTROL_2_VAR']' $FILE_VAR 'is created, suspending charging...'
            log -t batterylimit $CAPACITY_VAR':['$CONTROL_1_VAR':'$CONTROL_2_VAR']' $FILE_VAR 'is created, suspending charging...'
         else
-           #echo $CAPACITY_VAR':['$CONTROL_1_VAR':'$CONTROL_2_VAR'] suspending charging (1)...'
+           # echo $CAPACITY_VAR':['$CONTROL_1_VAR':'$CONTROL_2_VAR'] suspending charging (1)...'
            log -t batterylimit $CAPACITY_VAR':['$CONTROL_1_VAR':'$CONTROL_2_VAR'] suspending charging (1)...'
         fi  
+        INTERVAL_VAR=$INTERVAL_ON_VAR
       else
         # $CAPACITY_VAR -ge $MIN && $CAPACITY_VAR -le $MAX
         if [ -e $FILE_VAR ]
         then
            echo "1" > $CONTROL_FILE
            CONTROL_2_VAR=$(cat $CONTROL_FILE)
-           #echo $CAPACITY_VAR':['$CONTROL_1_VAR':'$CONTROL_2_VAR'] suspending charging (2)...'
+           # echo $CAPACITY_VAR':['$CONTROL_1_VAR':'$CONTROL_2_VAR'] suspending charging (2)...'
            log -t batterylimit $CAPACITY_VAR':['$CONTROL_1_VAR':'$CONTROL_2_VAR'] suspending charging (2)...'
+           INTERVAL_VAR=$INTERVAL_ON_VAR
         else
-          #echo $CAPACITY_VAR':['$CONTROL_1_VAR'] charging...'
+          # echo $CAPACITY_VAR':['$CONTROL_1_VAR'] charging...'
           log -t batterylimit $CAPACITY_VAR':['$CONTROL_1_VAR'] charging...'
+          INTERVAL_VAR=$INTERVAL_OFF_VAR
         fi  
       fi
     fi    
-    INTERVAL_VAR=$INTERVAL_ON_VAR    
   fi
   STATUS_VAR=$(cat $STATUS_FILE)
-  #echo 'Wait...' $INTERVAL_VAR's' $STATUS_VAR
+  # echo 'Wait...' $INTERVAL_VAR's' $STATUS_VAR
   log -t batterylimit 'Wait...' $INTERVAL_VAR's ['$STATUS_VAR':'$POWERED_VAR']'
   sleep $INTERVAL_VAR
 done
