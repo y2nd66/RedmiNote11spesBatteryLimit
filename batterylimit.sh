2d3c36cd8e24bda9be4616d02ec957684d8414fd
y2nd66 <77805980+y2nd66@users.noreply.github.com>
2023-08-24 09:38:55 -0400

    Add files via upload

--- batterylimit.sh
+++ batterylimit.sh
@@ -6,24 +6,21 @@ CONTROL_FILE=/sys/class/power_supply/battery/input_suspend
 CAPACITY_FILE=/sys/class/power_supply/battery/capacity
 STATUS_FILE=/sys/class/power_supply/battery/status
 FILE_VAR=/data/adb/batterylimit_var
+FILE_NON=/data/adb/batterylimit_non
 POWERED_FILE=/data/adb/batterylimit_file
 INTERVAL_ON_VAR=5
 INTERVAL_OFF_VAR=60
-
-# if [ -e $FILE_VAR ]
-# then
-#    rm $FILE_VAR
-#    echo $FILE_VAR 'is removed'
-# fi
 
 while true
 do  
   dumpsys battery | grep "AC powered: true" > $POWERED_FILE
   POWERED_VAR=$(cat $POWERED_FILE)
   POWERED_VAR=$(echo $POWERED_VAR)
+  # echo ${#POWERED_VAR}
   # log -t batterylimit ${#POWERED_VAR}
     
-  if [[ ${#POWERED_VAR} = "0" ]]  
+  if [[ ${#POWERED_VAR} = "0" ]] ||
+     [ -e $FILE_NON ]
   then
     INTERVAL_VAR=$INTERVAL_OFF_VAR  
   else
